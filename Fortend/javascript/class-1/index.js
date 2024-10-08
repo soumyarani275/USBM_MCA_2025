c74ac372912462fde9bbf40999b72af8fa49c414
Soumyarani Khillar <you@example.com>
2024-10-08 10:42:09 +0530

    Task

---
+++ Fortend/javascript/class-1/index.js
@@ -0,0 +1,85 @@
+// var name= "Soumya";
+
+// let age = 22;
+
+// const city = "Cuttack";
+
+// console.log(address);
+
+// var address = "Cuttack";
+
+// console.log(address);
+//null
+//let user
+// let user = null;
+// console.log(typeof null);
+// console.log(user);
+
+//undefined
+// let data;
+// console.log(typeof data);
+// console.log(data);
+
+//number
+// let num1 = 50;
+// let decimal_no= 1.0001;
+// let pi=3.14
+// let age = 25
+// let roll_no =23;
+
+// console.log(typeof num1); //number
+// console.log(typeof pi);
+
+//Addition
+// let num2 = 100;
+// let num3 = 125;
+// let addition = num2 + num3;
+// let substraction = num2 - num3;
+// let mul = num2 * num3;
+// let div = num2 / num3;
+// let mod = num2 % num3;
+// console.log(addition);
+// console.log(substraction);
+// console.log(mul);
+// console.log(div);
+// console.log(mod);
+
+//Bigint
+// let huge_no = 111111111111111111111111111111111155555555555555555555555555555;
+// console.log(typeof huge_no);
+
+//symbol
+// let sym1 = symbol("sym1");
+// let sym2 = symbol("sym2");
+// console.log(sym1 === sym2); //false
+
+//NAN (not a number)
+/*let num5 = 0;
+let num6 = 0;
+console.log( num5 / num6);
+
+let num7 = 9;
+let num8 = 10;
+console.log(num7 / num8);*/
+
+//boolean
+// let isMarried = False;
+// let isSingle = True;
+// console.log(typeof isMarried);
+// console.log(typeof isSingle);
+
+//Templete literal;
+let name = "soumya";
+let age = 23;
+let city = "Cuttack";
+let rollno = 125;
+let college = "USBM";
+let batch = "MCA";
+console.log(`my name is $(name)`);
+console.log(`my age is $(age)`);
+console.log(`my city is $(city)`);
+console.log(`my rollno is $(rollno)`);
+console.log(`my college is $(college)`);
+console.log(`my batch is $(batch)`);
+
+//to check 
