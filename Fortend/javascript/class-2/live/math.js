ac1fea61efc3954b31b287adc726e78896ff29b3
Soumyarani Khillar <khillarsoumyarani@gmail.com>
2024-10-08 12:31:28 +0530

    task

---
+++ Fortend/javascript/class-2/live/math.js
@@ -0,0 +1,81 @@
+//mathematical operatore:
+// let num1 =  5;
+// let num2 = 6;
+// let result = num1 + num2;
+// console.log(result);
+
+//relational operator :
+// to  compare two values
+// console.log(10>5);
+// console.log(10<5);
+// console.log(5>=5);
+// console.log(10>!5);
+// console.log(!5);
+// console.log(5==5);
+// console.log(5==6);
+// console.log(5=="5");
+
+//conditional
+//if(condition){
+//true code to be executed here
+// }
+
+//Falsy value :
+//false, 0, -0, "", ``, '', null, undefined, NaN
+//  
+
+ //task
+ // given total_bill, discount_start_price if you satisfie condition print account
+ //available otherwise print no discount
+//  let total_bill = 1000;
+//  if(total_bill>800){
+//  console.log("discount available");
+//  }else {
+//     console.log("no discount");
+//  }
+
+//  //check if (male) and then check 
+//  let age = 60;
+//  if(age >= 60){
+//     console.log("Adult and senior")
+//  }
+//  if(age >= 18){
+//     console.log("adult")
+//  }
+//  else{
+//     console.log("junior and child")
+//  }
+
+ //check age and marrige
+ let age = 30;
+ let male;
+ let gender;
+ if(gender===male){
+    if(age >= 22){
+        console.log("marrige");
+    }
+ } else {
+    console.log("not marrige");
+ }
+
+ //task : point all the voules (a, e,i ,o,u)
+ let char="a";
+ switch (char){
+    case "a":
+        console.log("it is a vowel");
+        break;
+    case "e":
+        conwsole.log("it is a vowel");
+        break;
+    case "i":
+        console.log("it is a vowel");
+        break;
+    case "o":
+        console.log("it is a vowel");
+        break;
+    default:
+        console.log("it is not vowel");
+        break;
+ }
+
+
